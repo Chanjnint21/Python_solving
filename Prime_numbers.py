850ffea740e8a1ce10a3543a699e9c48cde03e6f
Chanjnint21 <limchanjnint@gmail.com>
2022-07-11 17:43:17 +0700

    source code added

--- Prime_numbers.py
+++ Prime_numbers.py
@@ -1,11 +1,35 @@
 # Find prime number between two number
 
-n1 = int(input("Input num 01: "))
-n2 = int(input("Input num 02: "))
-for i in range (n1, n2+1):
-    for pm in range (2, i):
-        if i%pm == 0:
-            break
-        else:
-            print(i)
-            break
+def method1():
+    amount = 0
+    n1 = int(input("Input num 01: "))
+    n2 = int(input("Input num 02: "))
+    for num in range (n1, n2+1):
+        if num > 1:
+            for pn in range (2, i):
+                if num % pn == 0:
+                    break
+            else:                                # each of for can use after break
+                print(num)
+                amount = amount +1
+    print("1. Total of prime numbers:", amount)
+
+# With all() funtion
+def method2():
+    amount = 0
+    n1 = int(input("Input num 01: "))
+    n2 = int(input("Input num 02: "))
+    for num in range ( n1, n2):
+        if num > 1:
+            if all(num%i!=0 for i in range(2, num)):
+                print(num)
+                amount = amount + 1
+    print("2. Total of prime numbers:", amount)
+
+
+#Call funtion here
+method1()
+#method2()
+
+
+
